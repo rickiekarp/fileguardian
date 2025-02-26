5328197e0371b541ed870331ca69661240d92959
Rickie Karp <rickie.karp@yandex.com>
2025-02-26 23:22:34 +0100

    Make sure ConfigBaseDir is set to current working directory of binary

--- config/fileloader.go
+++ config/fileloader.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
@@ -20,6 +21,17 @@ var Conf ConfigFile
 // ReadConfigFile reads the given config file and tries to unmarshal it into the given configStruct
 func ReadConfigFile() error {
 
+	// if the ConfigBaseDir has not been set (e.g. from ldflags), set it here
+	if ConfigBaseDir == "" {
+		binaryDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logrus.Error("binaryDir.Get err: ", err)
+			return err
+		}
+		ConfigBaseDir = binaryDir + "/"
+	}
+
+	// read config file
 	yamlFile, err := os.ReadFile(ConfigBaseDir + "config.yaml")
 	if err != nil {
 		logrus.Error("yamlFile.Get err: ", err)
